41cf91533cc53bdab3417c0c6c9657868409e31d
Владислав <fayttter@gmail.com>
2026-03-03 22:42:11 +0200

    баланс додавання,віднімання, перевірка

---
+++ OOП/bank_account.py
@@ -0,0 +1,19 @@
+class BankAccount():
+    def __init__(self, name, balance):
+        self.name = name
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"Рахунок поповнено на {amount}, загальна сума {self.balance}.")
+
+    def withdraw(self, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+        else:
+            print("Недостатньо коштів!")
+
+
+user1 = BankAccount("Олексій", 2000)
+user1.deposit(500)
+user1.withdraw(2700)
