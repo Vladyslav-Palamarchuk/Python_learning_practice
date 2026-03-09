245e92a21fa4e362fd4b490813f9a04e788d0291
Владислав <fayttter@gmail.com>
2026-03-09 23:03:21 +0200

    додано монстра, бій через цикл while

--- game_heroVSmonster/create_game.py
+++ game_heroVSmonster/create_game.py
@@ -1,4 +1,4 @@
-from random import random
+
 from random import randint
 
 
@@ -8,15 +8,49 @@ class Character:
         self.hp = hp
         self.damage = damage
 
-    def si_alive(self):
+    def is_alive(self):
         return self.hp > 0
 
     def take_damage(self, amount):
         self.hp -= amount
         print(f'{self.name}, отримав {amount} шкоди! Залишилося хп {self.hp}. ')
 
+
 class Hero(Character):
     def attack(self, enemy):  #додали ворог
-        hit = random.randint(1, self.damage)
+        hit = randint(1, self.damage)
         print(f"{self.name}, атакує {enemy.name}!")
         enemy.take_damage(hit)
+
+    def heal(self):
+        self.hp += 20
+        print(f"{self.name} лікування! Поточне здоров'я {self.hp}")
+
+
+class Monster(Character):
+    def attack(self, enemy):  # додали ворог
+        hit = randint(1, self.damage)
+        print(f"{self.name}, атакує {enemy.name}!")
+        enemy.take_damage(hit)
+
+player = Hero("Лицар", 100, 21)
+dragon = Monster("Драконіс", 120, 20)
+
+
+while player.is_alive() and dragon.is_alive():
+    print("\n --------Твій хід --------")
+    action = input("1 - Атакувати, 2 - Лікуватися")
+
+    if action == "1":
+        player.attack(dragon)
+    elif action == "2":
+        player.heal()
+
+    if dragon.is_alive():
+        print("\n Монстр атакує у відповідь!")
+        dragon.attack(player)
+
+if player.is_alive():
+    print("Дракон переміг, перемога!")
+else:
+    print("Монстр переміг((")
